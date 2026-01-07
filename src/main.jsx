d3ec5302b539b70686472b3fdc0732f5c482cea6
Govinda Nawalkishor Boob <govindaboob11@gmail.com>
2026-01-07 14:29:24 +0530

    Fix Vercel asset caching - add proper cache headers and restore full app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App'
-import AppTest from './AppTest'
+import App from './App'
+// import AppTest from './AppTest'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppTest />
+    <App />
   </React.StrictMode>
 )
